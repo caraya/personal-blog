48258aa6b2e5e3b25d4b59d8ead46426d2ea37bb
Carlos Araya <carlos.araya@gmail.com>
2025-10-07 14:20:32 -0700

    Updated Eleventy to version 3.1

---
+++ _data/metadata.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	site_name: "The Publishing Project",
+	url: "https://publishing-project.rivendellweb.net/",
+	language: "en-US",
+	author: {
+		twitter_handle: "@elrond25",
+		name: "carlos araya",
+		email: "carlos.araya@gmail.com"
+	}
+}
